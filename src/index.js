c11fa8b37790eece1d75d3179a0d4ff1d0fd7f20
Jesse Brown <Brownjesse8895@yahoo.com>
2021-07-01 10:00:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+fetch("https://dog.ceo/api/breeds/image/random" )
+    .then(res => res.json)
+    .then(data => console.log(data))
+// fetches the images using the URL 
+// parses the response as JSON 
+//adds image element to the DOM for each image in the array 
