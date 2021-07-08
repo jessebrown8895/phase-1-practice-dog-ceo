a6552a196a0d3bc9041d0dd2c9c70dfb814694f6
Jesse Brown <Brownjesse8895@yahoo.com>
2021-07-08 14:19:53 -0500

    commit tree bab92eed9ae6911ae0f9398a351fb10299f0ec58

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
     } 
 })
 // challenge 2
+// on page load, fetch all the dog breeds using the url above
+/// adds the breeds to the page in the <ul> provided in the index.html
 const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
 fetch(breedUrl)
 .then(response => response.json())
@@ -25,15 +27,14 @@ fetch(breedUrl)
         const anElement = document.createElement('li')
          anElement.innerText = key
         dogBreedContain.append(anElement)
-    anElement.addEventListener("click", e => {
+    //Challenge 3 
+// create some JavaScript code that when the user clicks on any of the <li>'s 
+//the color of that <li> changes
+        anElement.addEventListener("click", e => {
         anElement.style.color = "red"
     })
     })
 })
-// on page load, fetch all the dog breeds using the url above
-/// adds the breeds to the page in the <ul> provided in the index.html
 
-//Challenge 3 
-// create some JavaScript code that when the user clicks on any of the <li>'s 
-//the color of that <li> changes
+
 
